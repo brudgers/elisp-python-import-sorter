9683887d43b9d057276ae9bf330dbd18f2d9d5ee
ben rudgers <ben.rudgers@gmail.com>
2017-04-25 16:50:31 -0500

    working on excursion for generating and editing a temporary buffer

--- python-import-sort.el
+++ python-import-sort.el
@@ -15,3 +15,47 @@ Returns a dotted list of: (import-statement . sort-term)."
            a-list))))
 
 <sort-import-statements-by-name>>
+
+;;; Copy Python Buffer
+
+;;; The match string for flushing and keeping
+(setq import-match "import")
+
+(defun buffer-flush-matching-lines (regex)
+  "Flushes matching lines from buffer."
+  (mark-whole-buffer)
+  (flush-lines regex))
+
+(defun buffer-flush-not-matching-lines (regex)
+  "Removes non-matching lines from buffer."
+    (mark-whole-buffer)
+    (keep-lines regex))
+
+(defun first-matching-line (regex)
+  "Finds the start of line for the first line matching regex."
+  (beginning-of-buffer)
+  (search-forward regex)
+  (move-beginning-of-line nil))
+
+;; use fundamental mode to avoid capturing fontlock information
+;; when calling buffer-string
+(fundamental-mode)
+(save-excursion
+  (first-matching-line import-match)
+  (let ((place-to-insert (point-marker))
+        (whole-buffer (buffer-string)))
+    ;; delete the matching lines from original buffer
+    (buffer-flush-matching-lines import-match)
+    (insert
+     (with-temp-buffer
+       (fundamental-mode)
+       (insert whole-buffer)
+       (buffer-flush-not-matching-lines import-match)
+       (let
+           ((statements (parse-import-statements (mark-whole-buffer))))
+         
+         
+       ))) ; end insert
+    ))
+;; restore default mode for file extension
+(normal-mode)
